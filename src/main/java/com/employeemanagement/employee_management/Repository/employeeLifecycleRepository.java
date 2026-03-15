7a3be7a2de7938ba47c42cc690bd0a83e911d895
Shiddhant9879 <sidraynite@gmail.com>
2026-03-15 09:45:04 +0530

    rewrote the repo for this file

--- src/main/java/com/employeemanagement/employee_management/Repository/employeeLifecycleRepository.java
+++ src/main/java/com/employeemanagement/employee_management/Repository/employeeLifecycleRepository.java
@@ -1,3 +1,11 @@
 package com.employeemanagement.employee_management.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.employeemanagement.employee_management.model.EmployeeLifecycle;
+import java.util.Optional;
+
+public interface employeeLifecycleRepository extends JpaRepository<EmployeeLifecycle, Long> {
+
+    Optional<EmployeeLifecycle> findByEmployeeId(Long employeeId);
+
+}
